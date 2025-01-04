c4e3405b0529dd49aee5ba62f43336459fdb349b
310310Z <ry1ak2sh3su4@gmail.com>
2025-01-04 15:17:57 +0900

    clear

--- nextjs-docker/my-nextjs-app/.next/server/pages/dashboard.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/dashboard.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_7ab27f._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__6cf3ad._.js");
-runtime.loadChunk("server/chunks/ssr/styles_globals_ff5908.css");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/dashboard.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- nextjs-docker/my-nextjs-app/.next/server/pages/task_manager.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/task_manager.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_067029._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__91f9fd._.js");
-runtime.loadChunk("server/chunks/ssr/styles_globals_ff5908.css");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/index.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
